ec3a42bfaefd31a3314472140d563cdff687a1d3
Bear M <bear.mistele@gmail.com>
2024-06-17 20:09:10 -0700

    Fixed Port to function with Render

--- Develop/server.js
+++ Develop/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 // Middleware to parse JSON
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -13,6 +15,6 @@ app.get('/notes', (req, res) => {
 });
 
 // Start the server
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Server is running on port 3000');
 });
