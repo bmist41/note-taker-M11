f4b7e38850d170938d8c94ed8782d8bda8b49d9c
Bear M <bear.mistele@gmail.com>
2024-06-17 21:42:29 -0700

    file restructure

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+app.use(express.urlencoded({ extended: false }));
+const app = express();
+
+// Middleware to parse JSON
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/api/notes', require('./routes/apiRoutes'));
+app.use(express.json());
+
+// Route for the notes page
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log('Server is running on port 3000');
+});
